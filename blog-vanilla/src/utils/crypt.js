df5020c7cfedc3ab60106e365cd48267ea3ca952
memory1668 <1163549075@qq.com>
2019-08-26 15:08:11 +0800

    加密

--- blog-vanilla/src/utils/crypt.js
+++ blog-vanilla/src/utils/crypt.js
@@ -7,7 +7,7 @@ const SECRET_KEY = 'Jab21_-#B88'//不可泄露和修改
 function md5(content){
     let md5 = crypto.createHash('md5')
     return md5.update(content).digest('hex')//输出16进制
-}
+}   
 
 //加密函数
 function genPassword(password) {
